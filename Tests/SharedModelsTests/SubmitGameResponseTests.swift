0bfe6b11eb7f49c635d7be7cc7ea1fb1c602e72f
Stephen Celis <stephen@stephencelis.com>
2021-04-12 13:46:26 -0400

    Add more codability tests for backwards compatibility

---
+++ Tests/SharedModelsTests/SubmitGameResponseTests.swift
@@ -0,0 +1,60 @@
+import FirstPartyMocks
+import Overture
+import SharedModels
+import TestHelpers
+import XCTest
+
+class SubmitGameResponseTests: XCTestCase {
+  func testSubmitGameResponse_Codability_DailyChallenge() throws {
+    try assertBackwardsCompatibleCodable(
+      value: SubmitGameResponse.dailyChallenge(
+        DailyChallengeResult(
+          outOf: 876,
+          rank: 15,
+          score: 5432,
+          started: true
+        )
+      ),
+      json: [
+        "dailyChallenge": [
+          "outOf": 876,
+          "rank": 15,
+          "score": 5432,
+          "started": true
+        ]
+      ]
+    )
+  }
+
+  func testSubmitGameResponse_Codability_Solo() throws {
+    try assertBackwardsCompatibleCodable(
+      value: SubmitGameResponse.solo(
+        LeaderboardScoreResult(
+          ranks: [
+            .allTime: .init(outOf: 87654, rank: 5432),
+            .lastDay: .init(outOf: 876, rank: 54),
+            .lastWeek: .init(outOf: 8765, rank: 543),
+          ]
+        )
+      ),
+      json: [
+        "solo": [
+          "ranks": [
+            "allTime": ["outOf": 87654, "rank": 5432],
+            "lastDay": ["outOf": 876, "rank": 54],
+            "lastWeek": ["outOf": 8765, "rank": 543],
+          ]
+        ]
+      ]
+    )
+  }
+
+  func testSubmitGameResponse_Codability_TurnBased() throws {
+    try assertBackwardsCompatibleCodable(
+      value: SubmitGameResponse.turnBased,
+      json: [
+        "turnBased": true
+      ]
+    )
+  }
+}
