8ebb9b7cec18805fd7727c8cbcb3548d0da46711
Brandon Williams <mbrandonw@hey.com>
2021-04-14 16:57:23 -0700

    Revert "temporarily remove leaderboard preview"
    This reverts commit a2c075a8582cc2a6bb54caa4860fb12bfc8303c2.

---
+++ App/Previews/LeaderboardsPreview/LeaderboardsPreviewApp.swift
@@ -0,0 +1,95 @@
+import ApiClient
+import CasePaths
+import ComposableArchitecture
+import LeaderboardFeature
+import Overture
+import SharedModels
+import Styleguide
+import SwiftUI
+
+@main
+struct LeaderboardsPreviewApp: App {
+  init() {
+    Styleguide.registerFonts()
+  }
+  
+  var body: some Scene {
+    var apiClient = ApiClient.noop
+    apiClient.override(
+      route: .leaderboard(
+        .fetch(
+          gameMode: .timed,
+          language: .en,
+          timeScope: .lastWeek
+        )
+      ),
+      withResponse: .ok(
+        FetchLeaderboardResponse(
+          entries: (1...20).map { index in
+            .init(
+              id: .init(rawValue: UUID()),
+              isSupporter: false,
+              isYourScore: false,
+              outOf: 2_000,
+              playerDisplayName: "Blob \(index)",
+              rank: index,
+              score: 4_000 - index * 100
+            )
+          }
+          + [
+            .init(
+              id: .init(rawValue: UUID()),
+              isSupporter: false,
+              isYourScore: true,
+              outOf: 2_000,
+              playerDisplayName: "Blob Sr.",
+              rank: 100,
+              score: 1_000
+            )
+          ]
+        )
+      )
+    )
+//      route: .leaderboard(
+//        .fetch(
+//          gameMode: .timed,
+//          language: .en,
+//          timeScope: .lastWeek
+//        )
+//      ),
+//      withResponse: Effect.ok(
+//        FetchLeaderboardResponse(
+//          entries: (1...20).map { index in
+//            .init(
+//              id: .init(rawValue: UUID()),
+//              isSupporter: false,
+//              isYourScore: false,
+//              outOf: 2_000,
+//              playerDisplayName: "Blob \(index)",
+//              rank: index,
+//              score: 4_000 - index * 100
+//            )
+//          }
+//        )
+//      )
+//      .delay(for: 1, scheduler: DispatchQueue.main)
+//      .eraseToEffect()
+//    )
+
+    return WindowGroup {
+      LeaderboardView(
+        store: .init(
+          initialState: .init(isHapticsEnabled: false, settings: .init()),
+          reducer: leaderboardReducer,
+          environment: .init(
+            apiClient: apiClient,
+            audioPlayer: .noop,
+            feedbackGenerator: .noop,
+            lowPowerMode: .`false`,
+            mainQueue: DispatchQueue.main.eraseToAnyScheduler()
+          )
+        )
+      )
+    }
+  }
+}
