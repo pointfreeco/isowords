07798df26f1dd28bb85a616942f5a0fd309c1e6e
Brandon Williams <mbrandonw@hey.com>
2021-04-15 07:58:53 -0700

    Add operator to defer starting an effect.

---
+++ Sources/TcaHelpers/Deferring.swift
@@ -0,0 +1,16 @@
+import ComposableArchitecture
+import Combine
+
+extension Effect {
+  public func deferred<S: Scheduler>(
+    for dueTime: S.SchedulerTimeType.Stride,
+    scheduler: S,
+    options: S.SchedulerOptions? = nil
+  ) -> Effect {
+    Just(())
+      .setFailureType(to: Failure.self)
+      .delay(for: dueTime, scheduler: scheduler, options: options)
+      .flatMap { self }
+      .eraseToEffect()
+  }
+}
