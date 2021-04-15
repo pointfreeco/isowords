3ded3be5bef2791cb477877325ab6b09ad70717b
Brandon Williams <135203+mbrandonw@users.noreply.github.com>
2021-04-15 13:20:23 -0700

    Remove matching helper now that swift-case-paths has it. (#95)

--- Sources/TcaHelpers/CasePathMatching.swift
+++ Sources/TcaHelpers/CasePathMatching.swift
@@ -1,9 +1,5 @@
 import CasePaths
 
-public func ~= <Root, Value>(pattern: CasePath<Root, Value>, value: Root) -> Bool {
-  pattern.extract(from: value) != nil
-}
-
 extension CasePath {
   public func isMatching(_ value: Root) -> Bool {
     self.extract(from: value) != nil
