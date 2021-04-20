5898f5c3e4f9930c2829f10649a7424ea4221ca3
Jasdev Singh <jasdevs@gmail.com>
2021-04-19 20:05:44 -0400

    Revert "Revert "Daily Challenge Quick Action. (#86) 1f88355d93766644f3e72ad95aa5135eddd8e521" (#96)"
    This reverts commit c2793f4aacbbe5273062ab2a064bb743b74bf9de.

--- Sources/AppFeature/AppDelegate.swift
+++ Sources/AppFeature/AppDelegate.swift
@@ -13,8 +13,13 @@ import UIKit
 import UserNotifications
 
 public enum AppDelegateAction: Equatable {
+  public enum SceneAction: Equatable {
+    case quickAction(type: String)
+  }
+
   case didFinishLaunching
   case didRegisterForRemoteNotifications(Result<Data, NSError>)
+  case scene(SceneAction)
   case userNotifications(UserNotificationClient.DelegateEvent)
   case userSettingsLoaded(Result<UserSettings, NSError>)
 }
@@ -144,5 +149,7 @@ let appDelegateReducer = Reducer<
         .subscribe(on: environment.mainQueue)
         .fireAndForget()
     )
+  case .scene:
+    return .none
   }
 }
