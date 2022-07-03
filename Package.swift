68b5ec218c50ad3b2a7d688806ed4f72f4e24c0b
Stephen Celis <stephen@stephencelis.com>
2022-07-03 11:43:20 -0400

    Fix

--- Package.swift
+++ Package.swift
@@ -171,7 +171,10 @@ var package = Package(
 // MARK: - client
 if ProcessInfo.processInfo.environment["TEST_SERVER"] == nil {
   package.dependencies.append(contentsOf: [
-    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.28.0")
+    .package(
+      url: "https://github.com/pointfreeco/swift-composable-architecture",
+      branch: "concurrency-updates"
+    )
   ])
   package.products.append(contentsOf: [
     .library(name: "ActiveGamesFeature", targets: ["ActiveGamesFeature"]),
