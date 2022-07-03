e7245c5bffd73f58003a03a1936c429d1d78dab1
Stephen Celis <stephen@stephencelis.com>
2022-07-03 14:34:20 -0400

    Audio player

--- Sources/AudioPlayerClient/Client.swift
+++ Sources/AudioPlayerClient/Client.swift
@@ -2,13 +2,21 @@ import ComposableArchitecture
 
 public struct AudioPlayerClient {
   public var load: ([Sound]) -> Effect<Never, Never>
+  public var loadAsync: @Sendable ([Sound]) async -> Void
   public var loop: (Sound) -> Effect<Never, Never>
+  public var loopAsync: @Sendable (Sound) async -> Void
   public var play: (Sound) -> Effect<Never, Never>
+  public var playAsync: @Sendable (Sound) async -> Void
   public var secondaryAudioShouldBeSilencedHint: () -> Bool
+  public var secondaryAudioShouldBeSilencedHintAsync: @Sendable () async -> Bool
   public var setGlobalVolumeForMusic: (Float) -> Effect<Never, Never>
+  public var setGlobalVolumeForMusicAsync: @Sendable (Float) async -> Void
   public var setGlobalVolumeForSoundEffects: (Float) -> Effect<Never, Never>
+  public var setGlobalVolumeForSoundEffectsAsync: @Sendable (Float) async -> Void
   public var setVolume: (Sound, Float) -> Effect<Never, Never>
+  public var setVolumeAsync: @Sendable (Sound, Float) async ->Void
   public var stop: (Sound) -> Effect<Never, Never>
+  public var stopAsync: @Sendable (Sound) async -> Void
 
   public struct Sound: Hashable {
     public let category: Category
@@ -39,13 +47,21 @@ public struct AudioPlayerClient {
 extension AudioPlayerClient {
   public static let noop = Self(
     load: { _ in .none },
+    loadAsync: { _ in },
     loop: { _ in .none },
+    loopAsync: { _ in },
     play: { _ in .none },
+    playAsync: { _ in },
     secondaryAudioShouldBeSilencedHint: { false },
+    secondaryAudioShouldBeSilencedHintAsync: { false },
     setGlobalVolumeForMusic: { _ in .none },
+    setGlobalVolumeForMusicAsync: { _ in },
     setGlobalVolumeForSoundEffects: { _ in .none },
+    setGlobalVolumeForSoundEffectsAsync: { _ in },
     setVolume: { _, _ in .none },
-    stop: { _ in .none }
+    setVolumeAsync: { _, _ in },
+    stop: { _ in .none },
+    stopAsync: { _ in }
   )
 }
 
@@ -55,20 +71,31 @@ extension AudioPlayerClient {
   extension AudioPlayerClient {
     public static let failing = Self(
       load: { _ in .failing("\(Self.self).load is unimplemented") },
+      loadAsync: XCTUnimplemented("\(Self.self).loadAsync"),
       loop: { _ in .failing("\(Self.self).loop is unimplemented") },
+      loopAsync: XCTUnimplemented("\(Self.self).loopAsync"),
       play: { _ in .failing("\(Self.self).play is unimplemented") },
-      secondaryAudioShouldBeSilencedHint: {
-        XCTFail("\(Self.self).secondaryAudioShouldBeSilencedHint is unimplemented")
-        return false
-      },
+      playAsync: XCTUnimplemented("\(Self.self).playAsync"),
+      secondaryAudioShouldBeSilencedHint: XCTUnimplemented(
+        "\(Self.self).secondaryAudioShouldBeSilencedHint", placeholder: false
+      ),
+      secondaryAudioShouldBeSilencedHintAsync: XCTUnimplemented(
+        "\(Self.self).secondaryAudioShouldBeSilencedHintAsync", placeholder: false
+      ),
       setGlobalVolumeForMusic: { _ in
         .failing("\(Self.self).setGlobalVolumeForMusic is unimplemented")
       },
+      setGlobalVolumeForMusicAsync: XCTUnimplemented("\(Self.self).setGlobalVolumeForMusicAsync"),
       setGlobalVolumeForSoundEffects: { _ in
         .failing("\(Self.self).setGlobalVolumeForSoundEffects is unimplemented")
       },
+      setGlobalVolumeForSoundEffectsAsync: XCTUnimplemented(
+        "\(Self.self).setGlobalVolumeForSoundEffectsAsync"
+      ),
       setVolume: { _, _ in .failing("\(Self.self).setVolume is unimplemented") },
-      stop: { _ in .failing("\(Self.self).stop is unimplemented") }
+      setVolumeAsync: XCTUnimplemented("\(Self.self).setVolumeAsync"),
+      stop: { _ in .failing("\(Self.self).stop is unimplemented") },
+      stopAsync: XCTUnimplemented("\(Self.self).stopAsync")
     )
   }
 #endif
--- Sources/AudioPlayerClient/Live.swift
+++ Sources/AudioPlayerClient/Live.swift
@@ -1,8 +1,10 @@
 import AVFoundation
+import ComposableArchitecture  // FIXME: Needed for UncheckedSendable
 
 extension AudioPlayerClient {
   public static func live(bundles: [Bundle]) -> Self {
-    Self(
+    let actor = AudioActor(bundles: .init(wrappedValue: bundles))
+    return Self(
       load: { sounds in
         .fireAndForget {
           queue.async {
@@ -22,6 +24,7 @@ extension AudioPlayerClient {
           }
         }
       },
+      loadAsync: { try? await actor.load(sounds: $0) },
       loop: { sound in
         .fireAndForget {
           queue.async {
@@ -29,6 +32,7 @@ extension AudioPlayerClient {
           }
         }
       },
+      loopAsync: { try? await actor.play(sound: $0, loop: true) },
       play: { sound in
         .fireAndForget {
           queue.async {
@@ -36,7 +40,11 @@ extension AudioPlayerClient {
           }
         }
       },
+      playAsync: { try? await actor.play(sound: $0) },
       secondaryAudioShouldBeSilencedHint: {
+        AVAudioSession.sharedInstance().secondaryAudioShouldBeSilencedHint
+      },
+      secondaryAudioShouldBeSilencedHintAsync: {
         AVAudioSession.sharedInstance().secondaryAudioShouldBeSilencedHint
       },
       setGlobalVolumeForMusic: { volume in
@@ -46,6 +54,7 @@ extension AudioPlayerClient {
           }
         }
       },
+      setGlobalVolumeForMusicAsync: { await actor.setMusicVolume(to: $0) },
       setGlobalVolumeForSoundEffects: { volume in
         .fireAndForget {
           queue.async {
@@ -53,6 +62,7 @@ extension AudioPlayerClient {
           }
         }
       },
+      setGlobalVolumeForSoundEffectsAsync: { await actor.setSoundEffectsVolume(to: $0) },
       setVolume: { sound, volume in
         .fireAndForget {
           queue.async {
@@ -60,14 +70,143 @@ extension AudioPlayerClient {
           }
         }
       },
+      setVolumeAsync: { try? await actor.setVolume(of: $0, to: $1) },
       stop: { sound in
         .fireAndForget {
           queue.async {
             files[sound]?.stop()
           }
         }
-      }
+      },
+      stopAsync: { try? await actor.stop(sound: $0) }
     )
+  }
+
+  private actor AudioActor {
+    enum Failure: Error {
+      case bufferInitializationFailed
+      case soundNotLoaded(AudioPlayerClient.Sound)
+      case soundsNotLoaded([AudioPlayerClient.Sound: Error])
+    }
+
+    enum Player {
+      case music(AVAudioPlayer)
+      case soundEffect(AVAudioPlayerNode, AVAudioPCMBuffer)
+    }
+
+    let audioEngine: AVAudioEngine
+    let bundles: [Bundle]
+    var players: [Sound: Player] = [:]
+    let soundEffectsNode: AVAudioMixerNode
+
+    init(bundles: UncheckedSendable<[Bundle]>) {
+      let audioEngine = AVAudioEngine()
+      let soundEffectsNode = AVAudioMixerNode()
+      audioEngine.attach(soundEffectsNode)
+      audioEngine.connect(soundEffectsNode, to: audioEngine.mainMixerNode, format: nil)
+      self.audioEngine = audioEngine
+      self.bundles = bundles.wrappedValue
+      self.soundEffectsNode = soundEffectsNode
+    }
+
+    func load(sounds: [Sound]) throws {
+      let sounds = sounds.filter { !self.players.keys.contains($0) }
+      try AVAudioSession.sharedInstance().setCategory(.ambient)
+      try AVAudioSession.sharedInstance().setActive(true, options: [])
+      var errors: [Sound: Error] = [:]
+      for sound in sounds {
+        for bundle in self.bundles {
+          do {
+            guard let url = bundle.url(forResource: sound.name, withExtension: "mp3")
+            else { continue }
+            switch sound.category {
+            case .music:
+              self.players[sound] = try .music(AVAudioPlayer(contentsOf: url))
+
+            case .soundEffect:
+              let file = try AVAudioFile(forReading: url)
+              guard
+                let buffer = AVAudioPCMBuffer(
+                  pcmFormat: file.processingFormat,
+                  frameCapacity: AVAudioFrameCount(file.length)
+                )
+              else { throw Failure.bufferInitializationFailed }
+              try file.read(into: buffer)
+              let node = AVAudioPlayerNode()
+              audioEngine.attach(node)
+              audioEngine.connect(node, to: soundEffectsNode, format: nil)
+              self.players[sound] = .soundEffect(node, buffer)
+            }
+          } catch {
+            errors[sound] = error
+          }
+        }
+      }
+      guard errors.isEmpty else { throw Failure.soundsNotLoaded(errors) }
+    }
+
+    func play(sound: Sound, loop: Bool = false) throws {
+      guard let player = self.players[sound] else { throw Failure.soundNotLoaded(sound) }
+
+      switch player {
+      case let .music(player):
+        player.numberOfLoops = loop ? -1 : 0
+        player.play(atTime: 0)
+
+      case let .soundEffect(node, buffer):
+        if !self.audioEngine.isRunning {
+          try audioEngine.start()
+        }
+
+        node.stop()  // TODO: Is this needed?
+        node.scheduleBuffer(
+          buffer,
+          at: nil,
+          options: loop ? .loops : [],
+          completionCallbackType: .dataPlayedBack,
+          completionHandler: nil
+        )
+        node.play()  // TODO: Is this needed?
+      }
+    }
+
+    func stop(sound: Sound) throws {
+      guard let player = self.players[sound] else { throw Failure.soundNotLoaded(sound) }
+
+      switch player {
+      case let .music(player):
+        player.setVolume(0, fadeDuration: 2.5)
+        Task {
+          try await Task.sleep(nanoseconds: 2_500 * NSEC_PER_MSEC)
+          player.stop()
+        }
+
+      case let .soundEffect(node, _):
+        node.stop()
+      }
+    }
+
+    func setVolume(of sound: Sound, to volume: Float) throws {
+      guard let player = self.players[sound] else { throw Failure.soundNotLoaded(sound) }
+
+      switch player {
+      case let .music(player):
+        player.volume = volume
+
+      case let .soundEffect(node, _):
+        node.volume = volume
+      }
+    }
+
+    func setMusicVolume(to volume: Float) {
+      for (sound, _) in self.players where sound.category == .music {
+        try? self.setVolume(of: sound, to: volume)
+      }
+    }
+
+    func setSoundEffectsVolume(to volume: Float) {
+      self.soundEffectsNode.volume = 0.25 * volume
+    }
   }
 }
 
