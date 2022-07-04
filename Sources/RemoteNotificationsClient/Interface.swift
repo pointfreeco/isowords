3f2befdb5f0bc8cc40a62325f217d4a5adfe29c0
Stephen Celis <stephen@stephencelis.com>
2022-07-03 23:42:54 -0400

    Remote notifications

--- Sources/RemoteNotificationsClient/Interface.swift
+++ Sources/RemoteNotificationsClient/Interface.swift
@@ -2,15 +2,21 @@ import ComposableArchitecture
 
 public struct RemoteNotificationsClient {
   public var isRegistered: () -> Bool
+  public var isRegisteredAsync: @Sendable () async -> Bool
   public var register: () -> Effect<Never, Never>
+  public var registerAsync: @Sendable () async -> Void
   public var unregister: () -> Effect<Never, Never>
+  public var unregisterAsync: @Sendable () async -> Void
 }
 
 extension RemoteNotificationsClient {
   public static let noop = Self(
     isRegistered: { true },
+    isRegisteredAsync: { true },
     register: { .none },
-    unregister: { .none }
+    registerAsync: {},
+    unregister: { .none },
+    unregisterAsync: {}
   )
 }
 
@@ -23,8 +29,11 @@ extension RemoteNotificationsClient {
         XCTFail("\(Self.self).isRegistered is unimplemented")
         return false
       },
+      isRegisteredAsync: XCTUnimplemented("\(Self.self).isRegisteredAsync", placeholder: false),
       register: { .failing("\(Self.self).register is unimplemented") },
-      unregister: { .failing("\(Self.self).unregister is unimplemented") }
+      registerAsync: XCTUnimplemented("\(Self.self).registerAsync"),
+      unregister: { .failing("\(Self.self).unregister is unimplemented") },
+      unregisterAsync: XCTUnimplemented("\(Self.self).unregisterAsync")
     )
   }
 #endif
--- Sources/RemoteNotificationsClient/Live.swift
+++ Sources/RemoteNotificationsClient/Live.swift
@@ -5,16 +5,19 @@
   extension RemoteNotificationsClient {
     public static let live = Self(
       isRegistered: { UIApplication.shared.isRegisteredForRemoteNotifications },
+      isRegisteredAsync: { await UIApplication.shared.isRegisteredForRemoteNotifications },
       register: {
         .fireAndForget {
           UIApplication.shared.registerForRemoteNotifications()
         }
       },
+      registerAsync: { await UIApplication.shared.registerForRemoteNotifications() },
       unregister: {
         .fireAndForget {
           UIApplication.shared.unregisterForRemoteNotifications()
         }
-      }
+      },
+      unregisterAsync: { await UIApplication.shared.unregisterForRemoteNotifications() }
     )
   }
 #elseif canImport(AppKit)
