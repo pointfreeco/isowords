1e479e62a9dc571901a98b9fce0d5f617d04f878
Stephen Celis <stephen@stephencelis.com>
2022-07-18 20:50:00 -0400

    wip

--- Sources/ComposableStoreKit/Live.swift
+++ Sources/ComposableStoreKit/Live.swift
@@ -15,8 +15,8 @@ extension StoreKitClient {
           let delegate = ProductRequest(continuation: continuation)
           request.delegate = delegate
           request.start()
-          continuation.onTermination = { _ in
-            request.cancel()
+          continuation.onTermination = { [request = UncheckedSendable(wrappedValue: request)] _ in
+            request.wrappedValue.cancel()
             _ = delegate
           }
         }
