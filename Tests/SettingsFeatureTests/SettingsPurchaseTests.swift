fd0e32e9ad2c09c2d41c0a769ddc1a19c5785007
Stephen Celis <stephen@stephencelis.com>
2022-07-29 12:22:12 -0400

    wip

--- Tests/SettingsFeatureTests/SettingsPurchaseTests.swift
+++ Tests/SettingsFeatureTests/SettingsPurchaseTests.swift
@@ -124,13 +124,13 @@ class SettingsPurchaseTests: XCTestCase {
 
     await store.receive(.paymentTransaction(.updatedTransactions([.restored])))
     await store.receive(.paymentTransaction(.removedTransactions([.restored])))
+    await store.receive(.currentPlayerRefreshed(.success(.blobWithPurchase))) {
+      $0.fullGamePurchasedAt = .mock
+    }
     await store.receive(
       .paymentTransaction(.restoreCompletedTransactionsFinished(transactions: [.restored]))
     ) {
       $0.isRestoring = false
-    }
-    await store.receive(.currentPlayerRefreshed(.success(.blobWithPurchase))) {
-      $0.fullGamePurchasedAt = .mock
     }
     await task.cancel()
   }
