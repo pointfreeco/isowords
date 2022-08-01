aac22f2f1ee76da0d5a74c8aadc6322acfc7e47d
Stephen Celis <stephen@stephencelis.com>
2022-08-01 16:03:13 -0400

    wip

--- Sources/TcaHelpers/OnChange.swift
+++ Sources/TcaHelpers/OnChange.swift
@@ -1,5 +1,33 @@
 import ComposableArchitecture
 
+extension ReducerProtocol {
+  public func onChange<LocalState: Equatable>(
+    of toLocalState: @escaping (State) -> LocalState,
+    perform additionalEffects: @escaping (LocalState, inout State, Action) -> Effect<
+      Action, Never
+    >
+  ) -> some ReducerProtocol<State, Action> {
+    self.onChange(of: toLocalState) { additionalEffects($1, &$2, $3) }
+  }
+
+  public func onChange<LocalState: Equatable>(
+    of toLocalState: @escaping (State) -> LocalState,
+    perform additionalEffects: @escaping (LocalState, LocalState, inout State, Action) -> Effect<
+      Action, Never
+    >
+  ) -> some ReducerProtocol<State, Action> {
+    Reduce { state, action in
+      let previousLocalState = toLocalState(state)
+      let effects = self.reduce(into: &state, action: action)
+      let localState = toLocalState(state)
+
+      return previousLocalState != localState
+        ? .merge(effects, additionalEffects(previousLocalState, localState, &state, action))
+        : effects
+    }
+  }
+}
+
 extension Reducer {
   public func onChange<LocalState>(
     of toLocalState: @escaping (State) -> LocalState,
