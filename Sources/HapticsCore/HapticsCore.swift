cd6db40161b58d720e29e68d0fcd01b7f4e49d27
Stephen Celis <stephen@stephencelis.com>
2022-09-22 11:55:04 -0700

    wip

--- Sources/HapticsCore/HapticsCore.swift
+++ Sources/HapticsCore/HapticsCore.swift
@@ -6,19 +6,19 @@ extension ReducerProtocol {
   public func haptics<Trigger: Equatable>(
     isEnabled: @escaping (State) -> Bool,
     triggerOnChangeOf trigger: @escaping (State) -> Trigger
-  ) -> Haptics<Self, Trigger> {
+  ) -> some ReducerProtocol<State, Action> {
     Haptics(base: self, isEnabled: isEnabled, trigger: trigger)
   }
 }
 
-public struct Haptics<Base: ReducerProtocol, Trigger: Equatable>: ReducerProtocol {
+private struct Haptics<Base: ReducerProtocol, Trigger: Equatable>: ReducerProtocol {
   let base: Base
   let isEnabled: (Base.State) -> Bool
   let trigger: (Base.State) -> Trigger
 
   @Dependency(\.feedbackGenerator) var feedbackGenerator
 
-  public var body: some ReducerProtocol<Base.State, Base.Action> {
+  var body: some ReducerProtocol<Base.State, Base.Action> {
     self.base.onChange(of: self.trigger) { _, _, state, _ in
       guard self.isEnabled(state) else { return .none }
       return .fireAndForget { await self.feedbackGenerator.selectionChanged() }
