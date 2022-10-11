c49492a8702d16fe94a091482bae5310eaa6c5eb
Stephen Celis <stephen@stephencelis.com>
2022-10-11 09:14:14 -0700

    wip

--- Package.swift
+++ Package.swift
@@ -28,13 +28,16 @@ var package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-crypto", from: "1.1.6"),
     .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.8.1"),
-    .package(url: "https://github.com/pointfreeco/swift-gen", from: "0.3.0"),
+    .package(
+      url: "https://github.com/pointfreeco/swift-composable-architecture",
+      branch: "agnostic-runtime-warn"
+    ),
     .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.1.0"),
+    .package(url: "https://github.com/pointfreeco/swift-gen", from: "0.3.0"),
     .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.9.2"),
     .package(url: "https://github.com/pointfreeco/swift-tagged", from: "0.6.0"),
     .package(url: "https://github.com/pointfreeco/swift-url-routing", from: "0.2.0"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.2.0"),
-    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.41.0"),
     .package(name: "Overture", url: "https://github.com/pointfreeco/swift-overture", from: "0.5.0"),
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.10.0"),
   ],
