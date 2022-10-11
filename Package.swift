62a73d163b59b4bbc14110b0d285d292f2c37601
Stephen Celis <stephen@stephencelis.com>
2022-10-11 10:35:16 -0700

    wip

--- Package.swift
+++ Package.swift
@@ -1017,7 +1017,7 @@ package.dependencies.append(contentsOf: [
   .package(url: "https://github.com/swift-server/swift-backtrace", .exact("1.2.0")),
   .package(url: "https://github.com/vapor/postgres-kit", .exact("2.2.0")),
   .package(url: "https://github.com/pointfreeco/swift-prelude", .revision("7ff9911")),
-  .package(url: "https://github.com/pointfreeco/swift-web", .revision("f4bb612")),
+  .package(url: "https://github.com/pointfreeco/swift-web", .revision("2ad82ec")),
 ])
 package.products.append(contentsOf: [
   .executable(name: "daily-challenge-reports", targets: ["daily-challenge-reports"]),
