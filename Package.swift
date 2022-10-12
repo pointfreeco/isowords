dc804e2e1a67e2086b6746837e41a2ba2acbc6ca
Stephen Celis <stephen@stephencelis.com>
2022-10-12 15:33:36 -0700

    bump

--- Package.swift
+++ Package.swift
@@ -28,10 +28,7 @@ var package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-crypto", from: "1.1.6"),
     .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "0.8.1"),
-    .package(
-      url: "https://github.com/pointfreeco/swift-composable-architecture",
-      branch: "agnostic-runtime-warn"
-    ),
+    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.42.0"),
     .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "0.1.0"),
     .package(url: "https://github.com/pointfreeco/swift-gen", from: "0.3.0"),
     .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.9.2"),
