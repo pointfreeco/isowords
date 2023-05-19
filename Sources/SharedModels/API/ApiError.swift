e06419819d86dcf0d9ddbe24222ac3741865648c
Malte Bünz <maltebuenz@posteo.de>
2023-05-19 21:09:30 +0200

    update ApiError
    - add failure reason
    - map NSError to failure reason
    - add offline reason

--- Sources/SharedModels/API/ApiError.swift
+++ Sources/SharedModels/API/ApiError.swift
@@ -5,6 +5,7 @@ public struct ApiError: Codable, Error, Equatable, LocalizedError {
   public let file: String
   public let line: UInt
   public let message: String
+  public var reason: FailureReason?
 
   public init(
     error: Error,
@@ -17,9 +18,38 @@ public struct ApiError: Codable, Error, Equatable, LocalizedError {
     self.file = String(describing: file)
     self.line = line
     self.message = error.localizedDescription  // TODO: separate user facing from debug facing messages?
+    switch (error as NSError).code {
+    case ErrorCode.connectionLost:
+      self.reason = .offline
+    default:
+      break
+    }
   }
 
   public var errorDescription: String? {
     self.message
+  }
+  
+  public var failureReason: String? {
+    self.reason?.description
+  }
+  
+  public static func == (lhs: Self, rhs: Self) -> Bool {
+    lhs.message == rhs.message && lhs.errorDump == rhs.errorDump
+  }
+}
+
+private enum ErrorCode {
+  static let connectionLost = -1005
+}
+
+public enum FailureReason: CustomStringConvertible, Equatable, Codable {
+  case offline
+  
+  public var description: String {
+    switch self {
+    case .offline:
+      return "Connection unavailable"
+    }
   }
 }
