2c34caf119050211913931495ef41386531d9257
Brandon Williams <mbrandonw@hey.com>
2023-08-15 08:28:49 -0400

    wip

--- Sources/TcaHelpers/EffectPrefix.swift
+++ Sources/TcaHelpers/EffectPrefix.swift
@@ -1,18 +1,18 @@
-import Combine
-import ComposableArchitecture
-
-extension EffectTask where Failure == Never {
-  public func prefix(id: AnyHashable, _ maxLength: Int) -> Self {
-    Deferred { () -> AnyPublisher<Output, Failure> in
-      if counts[id] ?? 0 > maxLength {
-        return Empty().eraseToAnyPublisher()
-      } else {
-        counts[id, default: 0] += 1
-        return self.eraseToAnyPublisher()
-      }
-    }
-    .eraseToEffect()
-  }
-}
-
-private var counts: [AnyHashable: Int] = [:]
+//import Combine
+//import ComposableArchitecture
+//
+//extension EffectTask where Failure == Never {
+//  public func prefix(id: AnyHashable, _ maxLength: Int) -> Self {
+//    Deferred { () -> AnyPublisher<Action, Failure> in
+//      if counts[id] ?? 0 > maxLength {
+//        return Empty().eraseToAnyPublisher()
+//      } else {
+//        counts[id, default: 0] += 1
+//        return self.eraseToAnyPublisher()
+//      }
+//    }
+//    .eraseToEffect()
+//  }
+//}
+//
+//private var counts: [AnyHashable: Int] = [:]
