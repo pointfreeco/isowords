816d53fcc2764a05f0f40664b2fe122744d1300e
Stephen Celis <stephen@stephencelis.com>
2023-12-07 16:19:45 -0800

    wip

--- Sources/SettingsFeature/SoundsSettingsView.swift
+++ Sources/SettingsFeature/SoundsSettingsView.swift
@@ -3,13 +3,7 @@ import Styleguide
 import SwiftUI
 
 struct SoundsSettingsView: View {
-  let store: StoreOf<Settings>
-  @ObservedObject var viewStore: ViewStoreOf<Settings>
-
-  init(store: StoreOf<Settings>) {
-    self.store = store
-    self.viewStore = ViewStore(self.store, observe: { $0 })
-  }
+  @Bindable var store: StoreOf<Settings>
 
   var body: some View {
     SettingsForm {
@@ -18,12 +12,10 @@ struct SoundsSettingsView: View {
           Text("Music volume")
 
           VStack {
-            Slider(
-              value: self.viewStore.$userSettings.musicVolume.animation(), in: 0...1
-            )
-            .accentColor(.isowordsOrange)
+            Slider(value: $store.userSettings.musicVolume.animation(), in: 0...1)
+              .accentColor(.isowordsOrange)
 
-            if self.viewStore.userSettings.musicVolume <= 0 {
+            if store.userSettings.musicVolume <= 0 {
               Text("Music is off")
                 .foregroundColor(.gray)
                 .adaptiveFont(.matterMedium, size: 14)
@@ -38,13 +30,10 @@ struct SoundsSettingsView: View {
           Text("Sound FX volume")
 
           VStack {
-            Slider(
-              value: self.viewStore.$userSettings.soundEffectsVolume.animation(),
-              in: 0...1
-            )
-            .accentColor(.isowordsOrange)
+            Slider(value: $store.userSettings.soundEffectsVolume.animation(), in: 0...1)
+              .accentColor(.isowordsOrange)
 
-            if self.viewStore.userSettings.soundEffectsVolume <= 0 {
+            if store.userSettings.soundEffectsVolume <= 0 {
               Text("Sound FX are off")
                 .foregroundColor(.gray)
                 .adaptiveFont(.matterMedium, size: 14)
